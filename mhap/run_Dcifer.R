c642ca2ae22a0ce534bf49786629275a4c10e38d
Edwin Sutanto <edwin.s-tan@outlook.com>
2024-11-28 10:59:04 +0700

    Move commands into functions for running Dcifer
    Move commands not relevant to the user to functions

--- mhap/run_Dcifer.R
+++ mhap/run_Dcifer.R
@@ -1,6 +1,76 @@
 #!/usr/bin/env Rscript
 library(dcifer)
 
+## Load all functions by running everything from line 5-70
+get.m1.estimate <- function(dres0) {
+  dmat <- dres0[, , "estimate"]
+  m1.estimate <- as.data.frame(as.table(dmat))
+  m1.estimate <- m1.estimate[!is.na(m1.estimate[["Freq"]]), ]
+  names(m1.estimate) <- c("sample_id2", "sample_id1", "M1")
+  
+  return(m1.estimate)
+}
+
+
+analyse.significantly.related.samples <-
+  function(dsmp, coi, afreq, dres0, alpha = 0.05) {
+    isig <- which(dres0[, , "p_value"] <= alpha, arr.ind = TRUE)[, 2:1] 
+    
+    revals <- mapply(generateReval, 1:5, nr = c(1e3, 1e2, 32, 16, 12))
+    
+    sig2 <- vector("list", nrow(isig))
+    for (i in 1:nrow(isig)) {
+      sig2[[i]] <- ibdEstM(dsmp[isig[i, ]], coi[isig[i, ]], afreq, equalr = TRUE)
+    }
+    M2 <- sapply(sig2, length)
+    rtotal2 <- sapply(sig2, sum)
+    
+    samples <- names(dsmp)
+    sig <- data.frame(
+      sample_id1 = samples[isig[, 1]],
+      sample_id2 = samples[isig[, 2]],
+      M = M2,
+      rtotal = rtotal2
+    )
+    
+    return(sig)
+  }
+
+
+calculate.overall.relatedness.estimate <- function(m1.estimate, sig, coi) {
+  mall.estimate <- merge(m1.estimate, sig, all = TRUE)
+  
+  coi.df <-
+    data.frame(
+      sample_id = sapply(strsplit(names(coi), ".", fixed = TRUE), "[[", 1),
+      coi = coi,
+      row.names = NULL
+    )
+  
+  mall.estimate <-
+    merge(mall.estimate, coi.df, by.x = "sample_id2", by.y = "sample_id")
+  mall.estimate <-
+    merge(
+      mall.estimate,
+      coi.df,
+      by.x = "sample_id1",
+      by.y = "sample_id",
+      suffixes = c("2", "1")
+    )
+  
+  mall.estimate[["scaled_r"]] <-
+    mall.estimate[["rtotal"]] /
+    (pmin(mall.estimate[["coi1"]], mall.estimate[["coi2"]]))
+  
+  mall.estimate[["relatedness"]] <- mall.estimate[["scaled_r"]]
+  mall.estimate[is.na(mall.estimate[["relatedness"]]), "relatedness"] <-
+    mall.estimate[is.na(mall.estimate[["relatedness"]]), "M1"]
+  
+  return(mall.estimate)
+}
+
+
+## Run the commands below step-by-step
 ## Change FIXME lines to the appropriate parameters
 
 # FIXME: change to path of filtered microhaplotype data
@@ -15,60 +85,15 @@ afreq <- calcAfreq(dsmp, coi)
 ## only one pair of strains between two infections can be related
 dres0 <- ibdDat(dsmp, coi, afreq)
 
-dmat <- dres0[, , "estimate"]
-m1.estimate <- as.data.frame(as.table(dmat))
-m1.estimate <- m1.estimate[!is.na(m1.estimate[["Freq"]]), ]
-names(m1.estimate) <- c("sample_id2", "sample_id1", "M1")
+m1.estimate <- get.m1.estimate(dres0)
 
 
 ## allow multiple pairs of strains to be related between two infections
-alpha <- 0.05
-isig <- which(dres0[, , "p_value"] <= alpha, arr.ind = TRUE)[, 2:1] 
-
-revals <- mapply(generateReval, 1:5, nr = c(1e3, 1e2, 32, 16, 12))
-
-sig2 <- vector("list", nrow(isig))
-for (i in 1:nrow(isig)) {
-  sig2[[i]] <- ibdEstM(dsmp[isig[i, ]], coi[isig[i, ]], afreq, equalr = TRUE)
-}
-M2 <- sapply(sig2, length)
-rtotal2 <- sapply(sig2, sum)
-
-samples <- names(dsmp)
-sig <- data.frame(
-  sample_id1 = samples[isig[, 1]],
-  sample_id2 = samples[isig[, 2]],
-  M = M2,
-  rtotal = rtotal2
-)
+sig <- analyse.significantly.related.samples(dsmp, coi, afreq, dres0)
 
 
 ## combine results from restrained and unrestrained M
-mall.estimate <- merge(m1.estimate, sig, all = TRUE)
-coi.df <-
-  data.frame(
-    sample_id = sapply(strsplit(names(coi), ".", fixed = TRUE), "[[", 1),
-    coi = coi,
-    row.names = NULL
-  )
-mall.estimate <-
-  merge(mall.estimate, coi.df, by.x = "sample_id2", by.y = "sample_id")
-mall.estimate <-
-  merge(
-    mall.estimate,
-    coi.df,
-    by.x = "sample_id1",
-    by.y = "sample_id",
-    suffixes = c("2", "1")
-  )
-
-mall.estimate[["scaled_r"]] <-
-  mall.estimate[["rtotal"]] /
-  (pmin(mall.estimate[["coi1"]], mall.estimate[["coi2"]]))
-
-mall.estimate[["relatedness"]] <- mall.estimate[["scaled_r"]]
-mall.estimate[is.na(mall.estimate[["relatedness"]]), "relatedness"] <-
-  mall.estimate[is.na(mall.estimate[["relatedness"]]), "M1"]
+mall.estimate <- calculate.overall.relatedness.estimate(m1.estimate, sig, coi)
 
 
 # FIXME: change to path of between-infection relatedness estimate

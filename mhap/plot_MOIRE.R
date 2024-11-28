02c9e78aa2fe5a2c95f83efb9510c17b520e03f8
Edwin Sutanto <edwin.s-tan@outlook.com>
2024-11-28 13:30:47 +0700

    Update plots generated from MOIRE output
    For COI and effective COI, add data points alongside boxplots.
    For polyclonal prevalence, add numbers used for calculation.

--- mhap/plot_MOIRE.R
+++ mhap/plot_MOIRE.R
@@ -2,6 +2,33 @@
 library(readxl)
 library(ggplot2)
 
+## Load all functions by running everything from line 6-28
+calculate_polyclonal_prevalence <- function(coi_summary, metadata_group_column) {
+  n_polyclonal <-
+    aggregate(
+      coi_summary[["prob_polyclonal"]] > 0.5,
+      coi_summary[metadata_group_column],
+      sum
+    )
+  n_total <-
+    aggregate(
+      coi_summary[["prob_polyclonal"]],
+      coi_summary[metadata_group_column],
+      length
+    )
+  
+  polyclonal_prevalence <-
+    merge(n_polyclonal, n_total, by = metadata_group_column)
+  names(polyclonal_prevalence)[2:3] <- c("n_polyclonal", "n_total")
+  
+  polyclonal_prevalence[["pc_polyclonal"]] <-
+    polyclonal_prevalence[["n_polyclonal"]] / polyclonal_prevalence[["n_total"]] * 100
+  
+  return(polyclonal_prevalence)
+}
+
+
+## Run the commands below step-by-step
 ## Change FIXME lines to the appropriate parameters
 
 # FIXME: change to path to metadata
@@ -55,7 +82,8 @@ ggplot(
   coi_summary,
   aes(x = as.factor(.data[[metadata_group_column]]), y = post_coi_mean)
 ) +
-  geom_boxplot() +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(width = 0.35, height = 0, alpha = 0.5) +
   xlab(metadata_group_column) +
   ylab("COI") +
   theme_classic() +
@@ -64,6 +92,9 @@ ggplot(
 dev.off()
 
 
+polyclonal_prevalence <-
+  calculate_polyclonal_prevalence(coi_summary, metadata_group_column)
+
 # FIXME: adjust name and size (in inches) of polyclonal prevalence plot
 polyclonal_prevalence_plot_file <- "location/to/mhap_polyclonal_prevalence.pdf"
 
@@ -71,30 +102,35 @@ pdf(file = polyclonal_prevalence_plot_file, width = 7, height = 7)
 
 # FIXME: polyclonal prevalence plot framework, adjust metadata as necessary
 ggplot(
-  coi_summary,
-  aes(x = as.factor(.data[[metadata_group_column]]), fill = prob_polyclonal > 0.5)
+  polyclonal_prevalence,
+  aes(
+    x = as.factor(.data[[metadata_group_column]]),
+    y = pc_polyclonal,
+    label = paste0("(", n_polyclonal, "/", n_total, ")")
+  )
 ) +
-  geom_bar(position = "fill") +
+  geom_col() +
+  ylim(0, 100) +
   xlab(metadata_group_column) +
-  ylab("Proportion") +
-  labs(fill = "Polyclonal") +
+  ylab("% polyclonal") +
   theme_classic() +
-  theme(legend.position = "bottom")
+  geom_text(nudge_y = 2)
 
 dev.off()
 
 
-# FIXME: adjust name and size (in inches) of COI plot
+# FIXME: adjust name and size (in inches) of effective COI plot
 effective_coi_plot_file <- "location/to/mhap_effective_COI.pdf"
 
 pdf(file = effective_coi_plot_file, width = 7, height = 7)
 
-# FIXME: COI plot framework, adjust metadata as necessary
+# FIXME: Effective COI plot framework, adjust metadata as necessary
 ggplot(
   effective_coi_summary,
   aes(x = as.factor(.data[[metadata_group_column]]), y = post_effective_coi_mean)
 ) +
-  geom_boxplot() +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(width = 0.35, height = 0, alpha = 0.5) +
   xlab(metadata_group_column) +
   ylab("eMOI") +
   theme_classic() +

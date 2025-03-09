3226281c2d2117230bd629de995a38613c691c82
Edwin Sutanto <edwin.s-tan@outlook.com>
2025-03-09 23:55:04 +0700

    Create MCMC diagnostics from MOIRE run

--- mhap/run_MOIRE.R
+++ mhap/run_MOIRE.R
@@ -1,9 +1,55 @@
 #!/usr/bin/env Rscript
 
+## Load all functions by running everything from line 4-46
+plot.posterior.burnin.trace <- function(mcmc_results) {
+  for (i in seq_along(mcmc_results[["chains"]])) {
+    plot(
+      chain[["posterior_burnin"]],
+      type = "l",
+      main = paste0("Chain ", i),
+      xlab = "Iteration",
+      ylab = "Posterior (burn-in)"
+    )
+  }
+}
+
+plot.posterior.sample.trace <- function(mcmc_results) {
+  for (i in seq_along(mcmc_results[["chains"]])) {
+    plot(
+      chain[["posterior_sample"]],
+      type = "l",
+      main = paste0("Chain ", i),
+      xlab = "Iteration",
+      ylab = "Posterior (sample)"
+    )
+  }
+}
+
+plot.posterior.sample.density <- function(mcmc_results) {
+  for (i in seq_along(mcmc_results[["chains"]])) {
+    plot(
+      density(chain[["posterior_sample"]], bw = "SJ"),
+      main = paste0("Chain ", i),
+      ylab = "Posterior (sample) density"
+    )
+  }
+}
+
+plot.posterior.sample.acf <- function(mcmc_results) {
+  for (chain in mcmc_results[["chains"]]) {
+    acf(
+      chain[["posterior_sample"]],
+      main = paste0("Chain ", i),
+      ylab = "Posterior (sample) ACF"
+    )
+  }
+}
+
+
 ## Change FIXME lines to the appropriate parameters
 
-## If you already have the RDS from previous MOIRE MCMC,
-## skip until line 22
+## If you already have the mcmc_results saved as RDS,
+## you don't need to re-do run_mcmc (use readRDS() instead)
 
 # FIXME: change to path of filtered microhaplotype data
 sfile <- "location/to/mhap_filtered.tsv"
@@ -12,11 +58,47 @@ df <- read.delim(sfile)
 data <- moire::load_long_form_data(df)
 mcmc_results <- moire::run_mcmc(data, is_missing = data[["is_missing"]])
 
-
 # FIXME: change to path of MOIRE MCMC 
 file <- "location/to/mhap_MOIRE.rds"
 
 saveRDS(mcmc_results, file = file)
+
+
+## produce MCMC diagnostics for assessment, see:
+## https://sbfnk.github.io/mfiidd/mcmc_diagnostics.html
+
+nchain <- length(mcmc_results[["chains"]])
+m <- ceiling(sqrt(nchain))
+
+# FIXME: change to path of posterior (burn-in) trace plot
+posterior.burnin.trace.plot.file <- "location/to/mhap_MOIRE_burnin_trace.pdf"
+pdf(file = posterior.burnin.trace.plot.file, width = 7, height = 7)
+par(mfrow = c(m, m))
+plot.posterior.burnin.trace(mcmc_results)
+dev.off()
+
+# FIXME: change to path of posterior (sample) trace plot
+posterior.sample.trace.plot.file <- "location/to/mhap_MOIRE_sample_trace.pdf"
+pdf(file = posterior.sample.trace.plot.file, width = 7, height = 7)
+par(mfrow = c(m, m))
+plot.posterior.sample.trace(mcmc_results)
+dev.off()
+
+# FIXME: change to path of posterior (sample) density plot
+posterior.sample.density.plot.file <- "location/to/mhap_MOIRE_sample_density.pdf"
+pdf(file = posterior.sample.density.plot.file, width = 7, height = 7)
+par(mfrow = c(m, m))
+plot.posterior.sample.density(mcmc_results)
+dev.off()
+
+# FIXME: change to path of posterior (sample) acf plot
+posterior.sample.acf.plot.file <- "location/to/mhap_MOIRE_sample_acf.pdf"
+pdf(file = posterior.sample.acf.plot.file, width = 7, height = 7)
+par(mfrow = c(m, m))
+plot.posterior.sample.acf(mcmc_results)
+dev.off()
+
+## STOP and determine whether the MCMC mixes well
 
 
 # FIXME: change to path of COI summary

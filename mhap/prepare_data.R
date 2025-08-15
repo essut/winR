194952f547b7fcea597fa5a8ca005e0672463fee
Edwin Sutanto <edwin.s-tan@outlook.com>
2025-08-15 14:16:23 +0930

    Expand users ability to filter alleles based on proportion
    Users can now select to filter alleles based on proportion that
    was calculated compared to the total read-pairs per marker per
    sample or to the count of the major allele per marker per sample.

--- mhap/prepare_data.R
+++ mhap/prepare_data.R
@@ -196,7 +196,19 @@ select.markers <- function(long, keep.marker = "microhaplotype") {
 }
 
 
-.calculate.prop <- function(long) {
+.calculate.prop.major <- function(long) {
+  dlong.count <- aggregate(count ~ sample_id + locus, long, max)
+  names(dlong.count)[length(dlong.count)] <- "major"
+  
+  long.wprop <- merge(long, dlong.count, sort = FALSE)
+  long.wprop[["prop"]] <-
+    long.wprop[["count"]] / long.wprop[["major"]]
+  
+  long.wprop
+}
+
+
+.calculate.prop.total <- function(long) {
   dlong.count <- aggregate(count ~ sample_id + locus, long, sum)
   names(dlong.count)[length(dlong.count)] <- "total"
   
@@ -208,8 +220,14 @@ select.markers <- function(long, keep.marker = "microhaplotype") {
 }
 
 
-allele.proportion.filter <- function(long, allele.proportion.cutoff) {
-  long.wprop <- .calculate.prop(long)
+allele.proportion.filter <- function(long, allele.proportion.cutoff, on = "total") {
+  if (on %in% "total") {
+    long.wprop <- .calculate.prop.total(long)
+  } else if (on %in% "major") {
+    long.wprop <- .calculate.prop.major(long)
+  } else {
+    stop("Valid options are 'total' or 'major' allele")
+  }
   
   long.wprop[
     long.wprop[["prop"]] >= allele.proportion.cutoff,
@@ -325,7 +343,7 @@ allele.count.cutoff <- 5 # at least 5 read-pairs needed for each allele
 
 # FIXME: comment any lines below to disable certain filters
 mhap <- select.markers(long, keep.marker = "microhaplotype")
-mhap.filtered <- allele.proportion.filter(mhap, allele.proportion.cutoff)
+mhap.filtered <- allele.proportion.filter(mhap, allele.proportion.cutoff, on = "total")
 mhap.filtered <- allele.count.filter(mhap.filtered, allele.count.cutoff)
 # mhap.filtered <- minimum.total.filter(mhap.filtered, minimum.total.cutoff)
 mhap.filtered.nloci.per.sample <- calculate.remaining.nloci(mhap.filtered)
